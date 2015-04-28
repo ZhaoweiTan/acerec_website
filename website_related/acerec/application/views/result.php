b0bbb73682f00f389b04d9a635eba224a2cc2f3e
xiaonaoxin <729493595@qq.com>
2015-04-28 16:27:04 +0800

    4.28
    mainly for views/result.php

--- website_related/acerec/application/views/result.php
+++ website_related/acerec/application/views/result.php
@@ -2,6 +2,14 @@
 <link rel="stylesheet" href="<?php echo ROOT_PATH?>/css/Icicle.css">
 <script src="<?php echo ROOT_PATH?>/js/jit.js"></script>
 <script src="<?php echo ROOT_PATH?>/js/icicle.js"></script>
+<!--[if (gte IE 9)|!(IE)]><!-->
+<script src="<?php echo ROOT_PATH?>/js/jquery.min.js"></script>
+<script src="<?php echo ROOT_PATH?>/js/amazeui.js"></script>
+<!--<![endif]-->
+<!--[if lte IE 8 ]>
+<script src="http://libs.baidu.com/jquery/1.11.1/jquery.min.js"></script>
+<![endif]-->
+
 <div class="am-g am-g-fixed">
     <div class="am-g">
         <form class="am-form" action="<?php echo ROOT_FILE?>/result" method="get" name="searchform">
@@ -9,7 +17,7 @@
             <input class="am-form-field" type="text" name="q" value="<?php echo $keyword?>">
         </div>
         <div class="am-u-sm-4">
-            <input class="am-btn am-btn-primary" value="Search" type="submit">
+            <input class="am-btn am-btzfn-primary" value="Search" type="submit">
         </div>
         </form>
     </div>
@@ -18,24 +26,48 @@
 
         <p>Found <?php echo $result['response']['numFound']?> results.</p>
 
+            <?php $i=0;?>
 
-<?php
-        $taxonomy=file_get_contents(ROOT_PATH.'/data/taxonomy.json');
-        $taxonomy=json_decode($taxonomy);
-        foreach($result['response']['docs'] as $or):?>
-            <h3><a href="#"><?php echo $or['title'][0]?></a></h3>
-            <?php
-            foreach ($or['ieeeTerms'] as $kw):?>
-                <p class="info" onclick="changeroot('Node<?php echo array_search($kw, $taxonomy)+1?>')"><?php echo $kw?></p>
-            <p class="describe"></p>
-        <?php endforeach;?>
-        <?php endforeach;
-        echo "<p>".$page_link."</p>"
-?>
+            <?php////////////////view list/////////////////////////////////////////////?>
+
+            <div class="am-panel-group" id="accordion">
+                <div class="am-panel am-panel-default">
+                    <?php $taxonomy=file_get_contents(ROOT_PATH.'/data/taxonomy.json');
+                    $taxonomy=json_decode($taxonomy); ?>
+                    <?php foreach($result['response']['docs'] as $or):?>
+
+                        <?php $i=$i+1;?>
+
+
+                        <div class="am-panel-hd">
+                            <div class="am-panel-title" data-am-collapse="{parent: '#accordion', target:'#a<?php echo "$i"?>'}">
+                                <h3><a >
+                                        <?php echo $or['title'][0]?>
+                                    </a></h3>
+                            </div>
+                        </div>
+
+                        <div id="a<?php echo "$i"?>" class="am-panel-collapse am-collapse am-in">
+                            <div class="am-panel-bd">
+                                <?php foreach ($or['ieeeTerms'] as $kw):?>
+                                    <p class="info" onclick="changeroot('Node<?php echo array_search($kw, $taxonomy)+1?>')"><?php echo $kw?></p>
+                                    <p class="describe"></p>
+                                <?php endforeach;?>
+                            </div>
+                        </div>
+                        <script>
+                            $('#a<?php echo "$i"?>').collapse('close');
+                        </script>
+
+                    <?php endforeach;echo "<p>".$page_link."</p>"?>
+                </div>
+            </div>
+
+            <?php////////////////view list end/////////////////////////////////////////////?>
 
         </div>
         <div class="am-u-sm-9">
-            <section class="am-panel am-panel-default">
+            <section data-am-sticky class="am-panel am-panel-default">
                 <header class="am-panel-hd">
                     <h3 class="am-panel-title">Topic tree</h3>
                 </header>
